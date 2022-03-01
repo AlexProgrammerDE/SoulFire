427dbd53ead817e2d6f242b61b8b1479ea13db02
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-03-01 09:45:59 +0100

    Also set target

--- buildSrc/src/main/kotlin/sw.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/sw.java-conventions.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 tasks.withType<JavaCompile>() {
     options.encoding = "UTF-8"
