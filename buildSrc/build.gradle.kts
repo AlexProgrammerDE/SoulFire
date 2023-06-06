f4683c3129a01a226a60f2ff03a07e8306e44214
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-06-06 18:45:15 +0200

    Add code to export dependencies into a json file

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
-    // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     `kotlin-dsl`
 }
 
 repositories {
-    // Use the plugin portal to apply community plugins in convention plugins.
     gradlePluginPortal()
     mavenCentral()
 }
@@ -12,4 +10,6 @@ repositories {
 dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:8.0.0")
     implementation("gradle.plugin.org.cadixdev.gradle:licenser:0.6.1")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("org.apache.maven:maven-model:4.0.0-alpha-5")
 }
