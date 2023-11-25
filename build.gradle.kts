b41decf8af053ea1ea0f9b48f78f0df3c97bef95
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-11-25 17:16:23 +0100

    Apply publishing only in root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -158,6 +158,17 @@ dependencies {
     implementation(libs.protobuf.util)
 }
 
+val repoName = if (version.toString().endsWith("SNAPSHOT")) "maven-snapshots" else "maven-releases"
+publishing {
+    repositories {
+        maven("https://repo.codemc.org/repository/${repoName}/") {
+            credentials.username = System.getenv("CODEMC_USERNAME")
+            credentials.password = System.getenv("CODEMC_PASSWORD")
+            name = "codemc"
+        }
+    }
+}
+
 protobuf {
     protoc {
         artifact = "com.google.protobuf:protoc:${libs.versions.protoc.get()}"
--- buildSrc/src/main/kotlin/sw.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/sw.java-conventions.gradle.kts
@@ -69,17 +69,6 @@ indra {
     }
 }
 
-val repoName = if (version.toString().endsWith("SNAPSHOT")) "maven-snapshots" else "maven-releases"
-publishing {
-    repositories {
-        maven("https://repo.codemc.org/repository/${repoName}/") {
-            credentials.username = System.getenv("CODEMC_USERNAME")
-            credentials.password = System.getenv("CODEMC_PASSWORD")
-            name = "codemc"
-        }
-    }
-}
-
 /*
 val exportDependenciesTask by tasks.registering {
     group = "export"
