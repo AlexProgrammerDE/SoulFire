5810fa0b53f3693cf82e01e3a649cedc832cf7b5
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-12-28 11:44:42 +0100

    Fix en_us newline

--- data-generator/src/main/java/net/pistonmaster/serverwrecker/generator/generators/LanguageDataGenerator.java
+++ data-generator/src/main/java/net/pistonmaster/serverwrecker/generator/generators/LanguageDataGenerator.java
@@ -1,8 +1,5 @@
 package net.pistonmaster.serverwrecker.generator.generators;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import net.pistonmaster.serverwrecker.generator.util.ResourceHelper;
 
 public class LanguageDataGenerator implements IDataGenerator {
@@ -12,11 +9,7 @@ public class LanguageDataGenerator implements IDataGenerator {
     }
 
     @Override
-    public JsonElement generateDataJson() {
-        try {
-            return new Gson().fromJson(ResourceHelper.getResource("/assets/minecraft/lang/en_us.json"), JsonObject.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to generate language file", e);
-        }
+    public String generateDataJson() {
+        return ResourceHelper.getResource("/assets/minecraft/lang/en_us.json");
     }
 }
