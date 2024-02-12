c8ac2e67fbe13f373d7c7478f83c01e4602c630e
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-02-12 14:11:03 +0100

    Remove sourceset

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -36,16 +36,6 @@ tasks.withType<Checkstyle> {
     exclude("**/net/pistonmaster/soulfire/grpc/generated**")
 }
 
-// Inform IDEs like IntelliJ IDEA, Eclipse or NetBeans about the generated code.
-sourceSets {
-    main {
-        java {
-            srcDirs("${protobuf.generatedFilesBaseDir}/main/grpc")
-            srcDirs("${protobuf.generatedFilesBaseDir}/main/java")
-        }
-    }
-}
-
 idea {
     module {
         generatedSourceDirs.addAll(
