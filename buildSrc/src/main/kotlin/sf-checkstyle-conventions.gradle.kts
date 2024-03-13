f316f93875c7d071f920205650f54276be75c912
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-03-13 17:16:35 +0100

    Reactivate the checkstyle plugin

--- buildSrc/src/main/kotlin/sf-checkstyle-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/sf-checkstyle-conventions.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
-   // checkstyle
+    checkstyle
 }
 
-/*
 checkstyle {
     maxErrors = 0
     maxWarnings = 0
     toolVersion = libs.checkstyle.get().version.toString()
 }
 
-tasks.withType<Checkstyle> {
-    classpath = objects.fileCollection()
+// To fix a conflict in checkstyle dependencies
+configurations.checkstyle {
+    resolutionStrategy.capabilitiesResolution.withCapability("com.google.collections:google-collections") {
+        select("com.google.guava:guava:0")
+    }
 }
- */
