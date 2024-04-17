c8b53ebbe5f6f40393f1c2192882347db25a4857
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-04-17 11:38:29 +0200

    Some image building improvements

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker buildx build --push --platform linux/amd64 -t alexprogrammerde/soulfire .
+projectVersion=$(grep '^maven_version=' gradle.properties | cut -d'=' -f2)
+commitSha=$(git rev-parse --short HEAD)
+echo "Building image for commit $commitSha and version $projectVersion"
+
+docker buildx build \
+  --push \
+  --platform=linux/amd64,linux/arm64 \
+  -t alexprogrammerde/soulfire:$commitSha \
+  -t alexprogrammerde/soulfire:$projectVersion \
+  -t alexprogrammerde/soulfire:latest \
+  .
