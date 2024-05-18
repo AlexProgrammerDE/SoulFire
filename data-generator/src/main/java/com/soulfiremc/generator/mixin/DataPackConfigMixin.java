2352bd618e74fc54cac08591c219aadfba452351
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-18 10:06:57 +0200

    Properly enable all mc features in the server.properties file

--- data-generator/src/main/java/com/soulfiremc/generator/mixin/DataPackConfigMixin.java
+++ data-generator/src/main/java/com/soulfiremc/generator/mixin/DataPackConfigMixin.java
@@ -17,16 +17,19 @@
  */
 package com.soulfiremc.generator.mixin;
 
-import net.minecraft.world.flag.FeatureFlagSet;
+import java.util.List;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.DataPackConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FeatureFlagSet.class)
-public class FeatureFlagSetMixin {
-  @Inject(method = "isSubsetOf", at = @At("HEAD"), cancellable = true)
-  private void init(CallbackInfoReturnable<Boolean> cir) {
-    cir.setReturnValue(true);
+@Mixin(DataPackConfig.class)
+public class DataPackConfigMixin {
+  @Inject(method = "getEnabled", at = @At("HEAD"), cancellable = true)
+  public void getEnabled(CallbackInfoReturnable<List<String>> cir) {
+    cir.setReturnValue(FeatureFlags.REGISTRY.toNames(FeatureFlags.REGISTRY.allFlags()).stream().map(ResourceLocation::getPath).toList());
   }
 }
