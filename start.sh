cc218b92970d4b3a2de507d73c4eb9fdd11e1efb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-29 16:48:36 +0200

    Allow overriding jar file ran by the image

--- start.sh
+++ start.sh
@@ -5,5 +5,10 @@ if [ -z "$SF_RAM" ]; then
   SF_RAM="2G"
 fi
 
+if [ -z "$SF_JAR" ]; then
+  echo "SF_JAR is not set. Using default jar file of this image."
+  SF_JAR="/soulfire/soulfire.jar"
+fi
+
 echo "Starting SoulFire dedicated server..."
-java -Xmx$SF_RAM $SF_JVM_FLAGS -XX:+EnableDynamicAgentLoading -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar /soulfire/soulfire.jar
+java -Xmx$SF_RAM $SF_JVM_FLAGS -XX:+EnableDynamicAgentLoading -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar $SF_JAR
