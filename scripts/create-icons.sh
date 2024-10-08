5fa5b3159ec36afc7e10646b9912ea5ccb29826e
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-10-08 20:12:54 +0200

    Update icons

--- scripts/create-icons.sh
+++ scripts/create-icons.sh
@@ -4,34 +4,24 @@
 
 # png
 convert -version
-convert -background transparent -density 1000 -resize 512x512 \
-src/main/resources/icons/icon.svg \
-src/main/resources/icons/icon.png
+inkscape -z -o server/src/main/resources/icons/icon.png -w 512 -h 512 server/src/main/resources/icons/icon.svg
 
 # ico
 convert -background transparent -density 1000 \
 -define icon:auto-resize -colors 256 \
-src/main/resources/icons/icon.svg \
-src/main/resources/icons/icon.ico
+server/src/main/resources/icons/icon.png \
+server/src/main/resources/icons/icon.ico
 
 # icns
 convert -background transparent -density 1000 -resize 16x16 \
-src/main/resources/icons/icon.svg output_16x16.png
+server/src/main/resources/icons/icon.png output_16x16.png
 convert -background transparent -density 1000 -resize 32x32 \
-src/main/resources/icons/icon.svg output_32x32.png
+server/src/main/resources/icons/icon.png output_32x32.png
 convert -background transparent -density 1000 -resize 128x128 \
-src/main/resources/icons/icon.svg output_128x128.png
+server/src/main/resources/icons/icon.png output_128x128.png
 convert -background transparent -density 1000 -resize 256x256 \
-src/main/resources/icons/icon.svg output_256x256.png
+server/src/main/resources/icons/icon.png output_256x256.png
 convert -background transparent -density 1000 -resize 512x512 \
-src/main/resources/icons/icon.svg output_512x512.png
-png2icns src/main/resources/icons/icon.icns output_*.png
+server/src/main/resources/icons/icon.png output_512x512.png
+png2icns server/src/main/resources/icons/icon.icns output_*.png
 rm output_*.png
-
-# pkg background
-convert src/main/resources/icons/icon.png \
--background transparent -density 1000 -resize 103x123 -gravity center -extent 143x222 \
-installer/mac/SoulFire-background.png
-
-cp installer/mac/SoulFire-background.png \
-installer/mac/SoulFire-background-darkAqua.png
