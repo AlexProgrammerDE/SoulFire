836113525a3e4157655fa24e439d296ad0e83037
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-12-20 09:15:03 +0100

    Simplify rpc

--- server/src/main/java/com/soulfiremc/server/grpc/DownloadServiceImpl.java
+++ server/src/main/java/com/soulfiremc/server/grpc/DownloadServiceImpl.java
@@ -19,25 +19,33 @@ package com.soulfiremc.server.grpc;
 
 import com.google.protobuf.ByteString;
 import com.soulfiremc.grpc.generated.*;
+import com.soulfiremc.server.proxy.SFProxy;
 import com.soulfiremc.server.user.PermissionContext;
 import com.soulfiremc.server.util.ReactorHttpHelper;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Nullable;
 
 import java.net.URI;
 import java.util.UUID;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 @Slf4j
 public class DownloadServiceImpl extends DownloadServiceGrpc.DownloadServiceImplBase {
+  public static @Nullable SFProxy convertProxy(BooleanSupplier hasProxy, Supplier<ProxyProto> proxy) {
+    return hasProxy.getAsBoolean() ? SFProxy.fromProto(proxy.get()) : null;
+  }
+
   @Override
   public void download(DownloadRequest request, StreamObserver<DownloadResponse> responseObserver) {
     var instanceId = UUID.fromString(request.getInstanceId());
     ServerRPCConstants.USER_CONTEXT_KEY.get().hasPermissionOrThrow(PermissionContext.instance(InstancePermission.DOWNLOAD_URL, instanceId));
 
     try {
-      var proxy = RPCUtils.convertProxy(request::hasProxy, request::getProxy);
+      var proxy = convertProxy(request::hasProxy, request::getProxy);
       ReactorHttpHelper.createReactorClient(proxy, false)
         .headers(h -> request.getHeadersList().forEach(header -> h.set(header.getKey(), header.getValue())))
         .get()
