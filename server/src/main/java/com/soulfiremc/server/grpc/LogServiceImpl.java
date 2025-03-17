07e67c56b22a6ea6d1a77e905016d42637f5fedc
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-03-17 09:25:17 +0100

    Ensure getting permissions revoked means not receiving new logs

--- server/src/main/java/com/soulfiremc/server/grpc/LogServiceImpl.java
+++ server/src/main/java/com/soulfiremc/server/grpc/LogServiceImpl.java
@@ -132,12 +132,15 @@ public final class LogServiceImpl extends LogsServiceGrpc.LogsServiceImplBase {
 
     try {
       EventPredicate predicate = switch (request.getScopeCase()) {
-        case GLOBAL -> event -> true;
+        case GLOBAL -> event ->
+          ServerRPCConstants.USER_CONTEXT_KEY.get().hasPermission(PermissionContext.global(GlobalPermission.GLOBAL_SUBSCRIBE_LOGS));
         case INSTANCE -> {
           var instanceId = UUID.fromString(request.getInstance().getInstanceId());
-          yield event -> instanceId.equals(event.instanceId());
+          yield event ->
+            ServerRPCConstants.USER_CONTEXT_KEY.get().hasPermission(PermissionContext.instance(InstancePermission.INSTANCE_SUBSCRIBE_LOGS, instanceId))
+              && instanceId.equals(event.instanceId());
         }
-        case SCOPE_NOT_SET -> event -> false;
+        case SCOPE_NOT_SET -> throw new IllegalArgumentException("Scope not set");
       };
       new ConnectionMessageSender(
         subscribers,
