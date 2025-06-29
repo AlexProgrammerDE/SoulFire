49e19b59b375b44ca6c4bd1a9ee1cf04a8b47bd7
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-06-29 12:40:31 +0200

    fix: do not let mod override launcher dependencies

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -6,5 +6,7 @@ dependencies {
   libs.bundles.bom.get().forEach { api(platform(it)) }
 
   api(projects.shared)
-  runtimeOnly(project(":mod", "remapped"))
+  runtimeOnly(project(":mod", "remapped")) {
+    isTransitive = false
+  }
 }
