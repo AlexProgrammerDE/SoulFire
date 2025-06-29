f8a38014854d6c6ae8375948ca2023e466e050f0
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-06-29 20:17:49 +0200

    core: bump parchment

--- mod/build.gradle.kts
+++ mod/build.gradle.kts
@@ -48,7 +48,7 @@ unimined.minecraft {
   mappings {
     intermediary()
     mojmap()
-    parchment("1.21.5", "2025.06.15")
+    parchment("1.21.6", "2025.06.29")
 
     devFallbackNamespace("intermediary")
   }
