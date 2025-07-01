cbfc6a432886f70ddfcf284c956cd486fc8f9591
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-01 21:30:08 +0200

    feat: dynamically download based on http api

--- launcher/src/main/java/com/soulfiremc/launcher/SFMinecraftDownloader.java
+++ launcher/src/main/java/com/soulfiremc/launcher/SFMinecraftDownloader.java
@@ -17,42 +17,87 @@
  */
 package com.soulfiremc.launcher;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.SneakyThrows;
 import net.fabricmc.loader.impl.util.SystemProperties;
 
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 public class SFMinecraftDownloader {
-  private static final String JAR_NAME = "minecraft-client-1.21.7.jar";
-  private static final String JAR_URL = "https://piston-data.mojang.com/v1/objects/a2db1ea98c37b2d00c83f6867fb8bb581a593e07/client.jar";
+  private static final String MINECRAFT_VERSION = "1.21.7";
+  private static final String MINECRAFT_CLIENT_JAR_NAME = "minecraft-%s-client.jar".formatted(MINECRAFT_VERSION);
+  private static final String MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
+
+  private static Path getAndCreateDownloadDirectory(Path basePath) {
+    var downloadDir = basePath.resolve("mc-downloads");
+    if (!Files.exists(downloadDir)) {
+      try {
+        Files.createDirectories(downloadDir);
+      } catch (Exception e) {
+        throw new RuntimeException("Failed to create Minecraft download directory: " + downloadDir, e);
+      }
+    }
+    return downloadDir;
+  }
+
+  private static Path getMinecraftClientJarPath(Path basePath) {
+    return getAndCreateDownloadDirectory(basePath).resolve(MINECRAFT_CLIENT_JAR_NAME);
+  }
+
+  private static JsonObject getUrl(String url) {
+    try (var client = HttpClient.newHttpClient()) {
+      var response = client.send(HttpRequest.newBuilder(URI.create(url)).build(),
+        HttpResponse.BodyHandlers.ofString());
+      return JsonParser.parseString(response.body()).getAsJsonObject();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to fetch URL: " + url, e);
+    }
+  }
 
   @SuppressWarnings("unused")
   @SneakyThrows
   public static void loadAndInjectMinecraftJar(Path basePath) {
-    var mcJarsPath = basePath.resolve("mc-jars");
-    if (!Files.exists(mcJarsPath)) {
-      Files.createDirectories(mcJarsPath);
-    }
-
-    var minecraftJarPath = mcJarsPath.resolve(JAR_NAME);
-    if (!Files.exists(minecraftJarPath)) {
+    var minecraftJarPath = getMinecraftClientJarPath(basePath);
+    if (Files.exists(getMinecraftClientJarPath(basePath))) {
+      System.out.println("Minecraft already downloaded, continuing");
+    } else {
       System.out.println("Downloading Minecraft jar...");
-      var tempJarPath = Files.createTempFile("sf-mc-jar-download-", "-" + JAR_NAME);
-      try (var in = URI.create(JAR_URL).toURL().openStream()) {
+      var versionUrl = getUrl(MANIFEST_URL)
+        .getAsJsonArray("versions")
+        .asList()
+        .stream()
+        .map(JsonElement::getAsJsonObject)
+        .filter(v -> v.get("id").getAsString().equals(MINECRAFT_VERSION))
+        .map(v -> v.get("url").getAsString())
+        .findFirst()
+        .orElseThrow(() -> new RuntimeException("Minecraft version " + MINECRAFT_VERSION + " not found in manifest"));
+
+      var versionInfo = getUrl(versionUrl);
+      var clientUrl = versionInfo
+        .getAsJsonObject("downloads")
+        .getAsJsonObject("client")
+        .get("url")
+        .getAsString();
+
+      var tempJarPath = Files.createTempFile("sf-mc-jar-download-", "-" + MINECRAFT_CLIENT_JAR_NAME);
+      try (var in = URI.create(clientUrl).toURL().openStream()) {
         Files.copy(in, tempJarPath, StandardCopyOption.REPLACE_EXISTING);
       } catch (Exception e) {
         Files.deleteIfExists(tempJarPath);
-        throw new RuntimeException("Failed to download Minecraft jar from " + JAR_URL, e);
+        throw new RuntimeException("Failed to download Minecraft jar from " + clientUrl, e);
       }
 
       Files.copy(tempJarPath, minecraftJarPath);
       Files.deleteIfExists(tempJarPath);
       System.out.println("Minecraft jar downloaded and saved to: " + minecraftJarPath);
-    } else {
-      System.out.println("Minecraft jar already exists, skipping download.");
     }
 
     System.setProperty(SystemProperties.GAME_JAR_PATH_CLIENT, minecraftJarPath.toString());
