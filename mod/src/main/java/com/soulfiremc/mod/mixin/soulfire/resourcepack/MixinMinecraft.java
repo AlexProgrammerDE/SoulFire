8eb676575e730faf01c790551ce2a3c9acac763d
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-02 15:47:45 +0200

    fix: properly fake a reload finishing

--- mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/resourcepack/MixinMinecraft.java
+++ mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/resourcepack/MixinMinecraft.java
@@ -18,18 +18,34 @@
 package com.soulfiremc.mod.mixin.soulfire.resourcepack;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.PackResources;
+import net.minecraft.server.packs.resources.ReloadInstance;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
+import net.minecraft.util.Unit;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
-  @Inject(method = "reloadResourcePacks(ZLnet/minecraft/client/Minecraft$GameLoadCookie;)Ljava/util/concurrent/CompletableFuture;", at = @At(value = "HEAD"), cancellable = true)
-  private void onReloadResourcePacks(CallbackInfoReturnable<CompletableFuture<Void>> cir) {
-    // Do not trigger a resource pack load
-    cir.setReturnValue(CompletableFuture.completedFuture(null));
+  @Redirect(method = "reloadResourcePacks(ZLnet/minecraft/client/Minecraft$GameLoadCookie;)Ljava/util/concurrent/CompletableFuture;",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/ReloadableResourceManager;createReload(Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;Ljava/util/concurrent/CompletableFuture;Ljava/util/List;)Lnet/minecraft/server/packs/resources/ReloadInstance;"))
+  private ReloadInstance onReloadResourcePacks(ReloadableResourceManager instance, Executor backgroundExecutor, Executor gameExecutor, CompletableFuture<Unit> waitingFor, List<PackResources> resourcePacks) {
+    return new ReloadInstance() {
+      @Override
+      public @NotNull CompletableFuture<?> done() {
+        return CompletableFuture.completedFuture(null);
+      }
+
+      @Override
+      public float getActualProgress() {
+        return 1;
+      }
+    };
   }
 }
