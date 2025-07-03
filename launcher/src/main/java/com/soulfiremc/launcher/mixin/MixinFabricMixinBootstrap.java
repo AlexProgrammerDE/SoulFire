59585e82a6fefd37f25093ff5bb96be2dc6783bf
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-03 11:05:46 +0200

    fix: remap mixin refmaps in prod

---
+++ launcher/src/main/java/com/soulfiremc/launcher/mixin/MixinFabricMixinBootstrap.java
@@ -0,0 +1,35 @@
+/*
+ * SoulFire
+ * Copyright (C) 2024  AlexProgrammerDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.soulfiremc.launcher.mixin;
+
+import net.fabricmc.loader.impl.launch.FabricLauncher;
+import net.fabricmc.loader.impl.launch.FabricMixinBootstrap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@SuppressWarnings("UnusedMixin")
+@Mixin(FabricMixinBootstrap.class)
+public class MixinFabricMixinBootstrap {
+  @Redirect(method = "init",
+    at = @At(value = "INVOKE", target = "Lnet/fabricmc/loader/impl/launch/FabricLauncher;isDevelopment()Z"),
+    remap = false)
+  private static boolean isDevelopment(FabricLauncher instance) {
+    return true;
+  }
+}
