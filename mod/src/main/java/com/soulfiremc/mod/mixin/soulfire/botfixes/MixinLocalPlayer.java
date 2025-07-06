c6e6a00230fe26db2910530b50389e55637dc4e8
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-06 14:28:28 +0200

    fix: do now cause sprint or flying unintentionally

---
+++ mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/botfixes/MixinLocalPlayer.java
@@ -0,0 +1,44 @@
+/*
+ * SoulFire
+ * Copyright (C) 2024  AlexProgrammerDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.soulfiremc.mod.mixin.soulfire.botfixes;
+
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LocalPlayer.class)
+public class MixinLocalPlayer extends AbstractClientPlayer {
+  @Shadow
+  protected int sprintTriggerTime;
+
+  @SuppressWarnings("DataFlowIssue")
+  public MixinLocalPlayer() {
+    super(null, null);
+  }
+
+  @Inject(method = "aiStep", at = @At("HEAD"))
+  private void onAiStep(CallbackInfo ci) {
+    // Reset jump and sprint trigger times to prevent unwanted behavior with controls
+    this.jumpTriggerTime = 0;
+    this.sprintTriggerTime = 0;
+  }
+}
