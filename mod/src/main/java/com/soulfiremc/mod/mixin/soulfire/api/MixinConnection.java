ff5f314c861fbdf4c105e4831faa02d54e1a1a17
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-11 17:16:40 +0200

    feat: properly handle transfers

--- mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/api/MixinConnection.java
+++ mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/api/MixinConnection.java
@@ -19,7 +19,10 @@ package com.soulfiremc.mod.mixin.soulfire.api;
 
 import com.soulfiremc.server.protocol.BotConnection;
 import io.netty.channel.Channel;
+import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
 import net.minecraft.network.Connection;
+import net.minecraft.network.PacketListener;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,11 +32,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Connection.class)
 public class MixinConnection {
   @Shadow
+  @Nullable
+  private volatile PacketListener packetListener;
+  @Shadow
   private Channel channel;
 
   @Inject(method = "handleDisconnection", at = @At("RETURN"))
   public void onDisconnect(CallbackInfo ci) {
     if (this.channel != null && !this.channel.isOpen()) {
+      if (packetListener instanceof ClientCommonPacketListenerImpl commonListener
+        && commonListener.isTransferring) {
+        // Transfers aren't ordinary disconnects, we just ignore these
+        return;
+      }
+
       BotConnection.CURRENT.get().disconnect();
     }
   }
