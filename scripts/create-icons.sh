c8d465f5b5d6bca316bbb389a567124340100f7d
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-15 16:08:54 +0200

    feat: update logo

--- scripts/create-icons.sh
+++ scripts/create-icons.sh
@@ -2,5 +2,5 @@
 
 # png
 convert -version
-inkscape -z -o server/src/main/resources/icons/icon.png -w 512 -h 512 server/src/main/resources/icons/icon.svg
-inkscape -z -o server/src/main/resources/icons/pov_favicon.png -w 64 -h 64 server/src/main/resources/icons/icon.svg
+inkscape -z -o mod/src/main/resources/icons/icon.png -w 512 -h 512 mod/src/main/resources/icons/icon.svg
+inkscape -z -o mod/src/main/resources/icons/pov_favicon.png -w 64 -h 64 mod/src/main/resources/icons/icon.svg
