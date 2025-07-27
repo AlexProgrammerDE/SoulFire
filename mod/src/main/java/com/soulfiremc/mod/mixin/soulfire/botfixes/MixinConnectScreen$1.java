98a1d2c46d1bf34a477a937492cd85362c1a097f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-27 21:17:58 +0200

    fix: redirect errors properly to instance console

---
+++ mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/botfixes/MixinConnectScreen$1.java
@@ -0,0 +1,31 @@
+/*
+ * SoulFire
+ * Copyright (C) 2024  AlexProgrammerDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.soulfiremc.mod.mixin.soulfire.botfixes;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.soulfiremc.server.protocol.BotConnection;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(targets = "net.minecraft.client.gui.screens.ConnectScreen$1")
+public class MixinConnectScreen$1 {
+  @WrapMethod(method = "run")
+  private void wrapRun(Operation<Void> original) {
+    BotConnection.CURRENT.get().runnableWrapper().runWrapped(original::call);
+  }
+}
