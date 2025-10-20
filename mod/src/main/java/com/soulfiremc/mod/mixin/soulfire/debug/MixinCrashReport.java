360f86bd6cd0fe6132540df0731d2f08487c77d5
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-10-20 19:40:23 +0200

    fix: disable crash report stacktrace tracking
    we don't need that anyways with SF!

---
+++ mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/debug/MixinCrashReport.java
@@ -0,0 +1,36 @@
+/*
+ * SoulFire
+ * Copyright (C) 2024  AlexProgrammerDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.soulfiremc.mod.mixin.soulfire.debug;
+
+import net.minecraft.CrashReport;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CrashReport.class)
+public class MixinCrashReport {
+  @Shadow
+  private boolean trackingStackTrace;
+
+  @Inject(method = "<init>", at = @At(value = "RETURN"))
+  public void addCategory(CallbackInfo ci) {
+    this.trackingStackTrace = false;
+  }
+}
