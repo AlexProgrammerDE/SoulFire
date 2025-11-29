8e32d7049561184985d549a66541f884008c1000
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-11-29 17:13:59 +0100

    fix: make block break particles not cause warnings

---
+++ mod/src/main/java/com/soulfiremc/mod/mixin/headless/rendering/MixinBlockModelShaper.java
@@ -0,0 +1,40 @@
+/*
+ * SoulFire
+ * Copyright (C) 2024  AlexProgrammerDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.soulfiremc.mod.mixin.headless.rendering;
+
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockModelShaper.class)
+public class MixinBlockModelShaper {
+  @Inject(method = "getParticleIcon", at = @At("HEAD"), cancellable = true)
+  private void getParticleIcon(BlockState state, CallbackInfoReturnable<TextureAtlasSprite> cir) {
+    cir.setReturnValue(null);
+  }
+
+  @Inject(method = "getBlockModel", at = @At("HEAD"), cancellable = true)
+  private void getBlockModel(BlockState state, CallbackInfoReturnable<BlockStateModel> cir) {
+    cir.setReturnValue(null);
+  }
+}
