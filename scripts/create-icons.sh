c25948a81b4dcd9b9b1642c53d92788760fb2c6a
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-02-03 11:08:19 +0100

    chore: update icon variants

--- scripts/create-icons.sh
+++ scripts/create-icons.sh
@@ -2,5 +2,6 @@
 
 # png
 convert -version
-inkscape -z -o mod/src/main/resources/icons/icon.png -w 512 -h 512 mod/src/main/resources/icons/icon.svg
-inkscape -z -o mod/src/main/resources/icons/pov_favicon.png -w 64 -h 64 mod/src/main/resources/icons/icon.svg
+inkscape -z -o mod/src/main/resources/icons/icon-rounded.png -w 512 -h 512 mod/src/main/resources/icons/icon-rounded.svg
+inkscape -z -o mod/src/main/resources/icons/icon-square.png -w 512 -h 512 mod/src/main/resources/icons/icon-square.svg
+inkscape -z -o mod/src/main/resources/icons/pov_favicon.png -w 64 -h 64 mod/src/main/resources/icons/icon-rounded.svg
