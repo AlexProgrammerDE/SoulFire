1d81cbbcd3e8c736ec05283c27a29ca321bfb598
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-02-25 18:51:50 +0100

    fix: disable saving vfp configs

---
+++ mod/src/main/java/com/soulfiremc/mod/mixin/soulfire/viaversion/MixinAbstractSave.java
@@ -0,0 +1,33 @@
+/*
+ * SoulFire
+ * Copyright (C) 2026  AlexProgrammerDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.soulfiremc.mod.mixin.soulfire.viaversion;
+
+import com.viaversion.viafabricplus.save.AbstractSave;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractSave.class)
+public class MixinAbstractSave {
+  @Inject(method = "save", at = @At("HEAD"), cancellable = true, remap = false)
+  private void onSave(CallbackInfo ci) {
+    // No need to save anything, we save within SoulFire
+    ci.cancel();
+  }
+}
